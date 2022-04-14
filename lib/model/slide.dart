b08c963c72c39ff19a5a8e0fab55096718c61a7f
Jorge Zapata <jorgejaden@gmail.com>
2022-04-13 21:11:02 -0500

    last update yet??

--- lib/model/slide.dart
+++ lib/model/slide.dart
@@ -5,12 +5,14 @@ class Slide {
   final String title;
   final String desc;
   final String imageName;
+  final String linkName;
 
   Slide({
     required this.iconName,
     required this.title,
     required this.desc,
     required this.imageName,
+    required this.linkName,
   });
 }
 
@@ -21,6 +23,7 @@ final slideList = [
     desc:
         "We have highly qualified professionals with all the licenses to ensure a great result.",
     imageName: "assets/images/roofer_reasons.png",
+    linkName: "Roofing Service >",
   ),
   Slide(
     iconName: "assets/images/secure_icon.png",
@@ -28,6 +31,7 @@ final slideList = [
     desc:
         "From professional advice before starting the project to offering friendly financing options!",
     imageName: "assets/images/family_reasons.png",
+    linkName: "See More >",
   ),
   Slide(
     iconName: "assets/images/roof_icon.png",
@@ -35,5 +39,6 @@ final slideList = [
     desc:
         " Asphalt shingles are cost-effective and easy to install, maintain, and repair.",
     imageName: "assets/images/roof_reasons.png",
+    linkName: "Shingle Service >",
   ),
 ];
